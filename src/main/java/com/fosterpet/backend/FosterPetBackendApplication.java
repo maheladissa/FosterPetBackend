2f09d174e8f8206db77a6a7170b8b8ff516f9cc7
MahelaDissa <maheladissanayake@gmail.com>
2023-09-05 11:18:43 +0530

    Initial Commit

---
+++ src/main/java/com/fosterpet/backend/FosterPetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fosterpet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosterPetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FosterPetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fosterpet/backend/FosterPetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fosterpet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosterPetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
