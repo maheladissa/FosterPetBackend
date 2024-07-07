c1a4a6a9d2111f1c61270452a77235fe5ee86af5
MahelaDissa <maheladissanayake@gmail.com>
2024-07-07 21:29:39 +0530

    Added Invoice Generation

--- src/main/java/com/fosterpet/backend/payment/InvoiceRepository.java
+++ src/main/java/com/fosterpet/backend/payment/InvoiceRepository.java
@@ -3,6 +3,12 @@ package com.fosterpet.backend.payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvoiceRepository extends MongoRepository<Invoice, String>{
+    List<Invoice> findByBookingOwnerUserId(String userId);
+    List<Invoice> findByBookingKennelKennelID(String kennelId);
+    List<Invoice> findByBookingBookingID(String bookingId);
+    List<Invoice> findByBookingVolunteerVolunteerId(String volunteerId);
 }
